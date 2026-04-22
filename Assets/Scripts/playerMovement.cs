3e9e9d377b9519325aa0a703170cd4b1935b7954
srenegado <scottstationary@gmail.com>
2026-04-22 14:59:15 -0700

    Add basic left/right Player movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -3,10 +3,29 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Applying forces
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Note: Using Input is not that efficient e.g. if we wanted
+        // to use a controller or alternate keys
+
+        // Note: It's recommended to check for input in Update()
+        // rather than here in FixedUpdate() since Input is
+        // updated in Update(); there could be a slight delay here
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
